bd5f0c6cec915e7f18803e91a54d281cb8a4c47a
Euphoria <polunine28@gmail.com>
2022-11-25 17:06:45 +1100

    create exampleifelse

---
+++ examplec#/example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ната")
+{
+    Console.WriteLine("Ура, это же НАТА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
